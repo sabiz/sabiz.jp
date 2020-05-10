f24e40fbc88bb3fcfa9a142d8968b824dd98c6d7
sAbIz <bellett@sabiz.jp>
2020-05-10 13:15:39 +0900

    update.

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,4 +1,16 @@
 cd master
 npm ci
 npm run build
-pwd
+cd ..
+rm -rf gh-pages/*
+cp master/dist/* gh-pages/.
+cd gh-pages
+
+git config --global user.name "sabiz"
+git config --global user.email "sabiz@users.noreply.github.com"
+
+git remote set-url origin https://sabiz:${GITHUB_TOKEN}@github.com/sabiz/sabiz.jp.git
+
+git add .
+git commit -m"update."
+git push origin HEAD
